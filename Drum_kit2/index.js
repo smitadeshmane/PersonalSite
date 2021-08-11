7bff630e82d3151e501c2ee4874514ad832843a4
smitadeshmane <88760176+smitadeshmane@users.noreply.github.com>
2021-08-11 02:52:45 -0400

    Update index.js

--- Drum_kit2/index.js
+++ Drum_kit2/index.js
@@ -19,23 +19,23 @@ function charPlayDrums(char){
 
 switch(char){
   case("w"):
-  var tom1=new Audio('Drum_kit1/sounds/tom-1.mp3');
+  var tom1=new Audio('tom-1.mp3');
   tom1.play();
   break;
   case("a"):
-  var tom2=new Audio('Drum_kit1/sounds/tom-2.mp3');
+  var tom2=new Audio('tom-2.mp3');
   tom2.play();
   break;
   case("s"):
-  var tom3=new Audio('sounds/tom-3.mp3');
+  var tom3=new Audio('tom-3.mp3');
   tom3.play();
   break;
   case("d"):
-  var tom4=new Audio('sounds/tom-4.mp3');
+  var tom4=new Audio('tom-4.mp3');
   tom4.play();
   break;
   case("j"):
-  var snare=new Audio('sounds/snare.mp3');
+  var snare=new Audio('snare.mp3');
   snare.play();
   break;
   case("k"):
@@ -43,7 +43,7 @@ switch(char){
   crash.play();
   break;
   case("l"):
-  var kick=new Audio('sounds/kick-bass.mp3');
+  var kick=new Audio('kick-bass.mp3');
   kick.play();
   break;
   default:
